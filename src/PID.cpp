0e673c775ba55148bd30069169809f63e329e428
rxmeez <rameezkhan.1@hotmail.co.uk>
2017-12-06 16:16:20 +0000

    PID.cpp functions updated

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,37 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /*
+  * Initialize all the PID controller Coefficients with the input values as well as start all the error at 0.0
+  */
+
+  Kp = Kp;
+  Ki = Ki;
+  Kd = Kd;
+
+  p_error = 0.0;
+  d_error = 0.0;
+  i_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+  /*
+  * Update error values for each controls Coefficient (Kp, Ki, Kd)
+  */
+
+  // d_error the difference from cte and previous cte labeled as p_error
+  d_error = cte - p_error;
+
+  // p_error set it to new cte
+  p_error = cte;
+
+  // i_error continue to sum up all ctes
+  i_error += cte;
+
 }
 
 double PID::TotalError() {
+  // Returns the total error by combining all PID controller with their errors
+  return -Kp * p_error - Ki * i_error - Kd * d_error;
 }
-
